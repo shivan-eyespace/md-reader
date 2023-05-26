3f56bf3b33c7255fa9ce702f6da4895100bc39a2
Shivan Sivakumaran <shivan@eyespace.dev>
2023-05-26 15:10:16 +1200

    wip

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -5,20 +5,27 @@ use crossterm::{
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
-use std::{env::args, ffi::OsStr, fs::read_to_string, io, thread, time::Duration};
+use std::{
+    env::args,
+    ffi::OsStr,
+    fs::read_to_string,
+    io, thread,
+    time::{Duration, Instant},
+};
 use tui::{
     backend::{Backend, CrosstermBackend},
-    layout::{Constraint, Direction, Layout},
+    layout::Layout,
     style::{Color, Modifier, Style},
+    text::Spans,
     widgets::{Block, Borders, List, ListItem, ListState},
-    Terminal,
+    Frame, Terminal,
 };
 use walkdir::WalkDir;
 use yaml_rust::{scanner::ScanError, Yaml, YamlLoader};
 
 struct StatefulList<T> {
     state: ListState,
-    items,
+    items: Vec<T>,
 }
 
 impl<T> StatefulList<T> {
@@ -45,8 +52,8 @@ impl<T> StatefulList<T> {
     fn previous(&mut self) {
         let i = match self.state.selected() {
             Some(i) => {
-                if i == 0 {
-                    self.items.len() - 1;
+                if i <= 0 {
+                    self.items.len() - 1
                 } else {
                     i - 1
                 }
@@ -63,10 +70,14 @@ impl<T> StatefulList<T> {
 
 struct App<'a> {
     items: StatefulList<(&'a str, usize)>,
-    events: Vec<(&'a str, &'a str)>
 }
 
 impl<'a> App<'a> {
+    fn new() -> App<'a> {
+        App {
+            items: StatefulList::with_items(vec![("Item0", 1), ("Item1", 2)]),
+        }
+    }
 }
 
 struct MarkdownFile {
@@ -119,6 +130,63 @@ fn main() {
         markdown_files.len().to_string().blue()
     );
     _ = draw(markdown_files).unwrap();
+}
+
+fn run_app<B: Backend>(
+    terminal: &mut Terminal<B>,
+    mut app: App,
+    tick_rate: Duration,
+) -> io::Result<()> {
+    let mut last_tick = Instant::now();
+    loop {
+        terminal.draw(|f| ui(f, &mut app))?;
+
+        let timeout = tick_rate
+            .checked_sub(last_tick.elapsed())
+            .unwrap_or_else(|| Duration::from_secs(0));
+        if crossterm::event::poll(timeout)? {
+            if let Event::Key(key) = event::read()? {
+                match key.code {
+                    KeyCode::Char('q') => return Ok(()),
+                    KeyCode::Left => app.items.unselect(),
+                    KeyCode::Down => app.items.next(),
+                    KeyCode::Up => app.items.previous(),
+                    _ => {}
+                }
+            }
+        }
+        if last_tick.elapsed() >= tick_rate {
+            last_tick = Instant::now()
+        }
+    }
+}
+
+fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
+    let chunks = Layout::default();
+    let items: Vec<ListItem> = app
+        .items
+        .items
+        .iter()
+        .map(|i| {
+            let mut lines = vec![Spans::from(i.0)];
+            ListItem::new(lines).style(Style::default().fg(Color::Black).bg(Color::White))
+        })
+        .collect();
+
+    let items = List::new(items)
+        .block(Block::default().borders(Borders::ALL).title("List"))
+        .highlight_style(
+            Style::default()
+                .bg(Color::LightGreen)
+                .add_modifier(Modifier::BOLD),
+        )
+        .highlight_symbol(">> ");
+
+    f.render_stateful_widget(
+        items,
+        Layout::default().split(f.size())[0],
+        &mut app.items.state,
+    );
 }
 
 fn parse_frontmatter(text: &str) -> (Vec<Yaml>, String) {
@@ -174,7 +242,3 @@ fn draw(files: Vec<MarkdownFile>) -> Result<(), io::Error> {
     terminal.show_cursor()?;
     Ok(())
 }
-
-// TODO commit to github
-// Make a menu system - search?
-// Can go into md file, see the yaml frontmatter
